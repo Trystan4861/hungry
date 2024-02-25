629ba6453d4d64374121604969df43eee822fb43
Trystan4861 <trystan4861@gmail.com>
2024-02-25 15:38:18 +0100

    refactor + random inputid

--- src/components/MyFile.vue
+++ src/components/MyFile.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="file-select">
-    <input type="file" @change="handleFileChange" id="MyFile" :accept="accept">
-    <label class="btn btn-primary" for="MyFile">{{ text}}</label>
+  <div>
+    <input :id="inputID"  type="file" :accept="accept" @change="handleFileChange">
+    <label :for="inputID" class="btn btn-primary">{{ text}}</label>
   </div>
 </template>
 
@@ -16,18 +16,19 @@ export default {
     accept:{ type: String, default: "*.*" },
     forceFileName:{ type: Boolean, default: false }
   },
+  data(){
+    return {
+      inputID: `input-${Math.random().toString(36).slice(2)}`,
+    }
+  },
   setup(props, {emit}) {
-    const readFile = (inputFile) => new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = (event) =>{
-        try {  
-          const json = JSON.parse(event.target.result); 
-          resolve(json)
-        } catch (e) {  
-          reject("El archivo a importar debe ser un archivo de configuración de «Hungry!» válido")
-        }
+    const readFile  = (inputFile) => new Promise((resolve, reject) => {
+      const reader  = new FileReader();
+      reader.onload = (event) => {
+        try       { resolve(JSON.parse(event.target.result)) }
+        catch (e) { reject("El archivo a importar debe ser un archivo de configuración de «Hungry!» válido") }
       }
-      reader.onerror = (event) =>reject(event.target.error);
+      reader.onerror = (event) => reject(event.target.error);
       reader.readAsText(inputFile);
     });
 
@@ -35,11 +36,8 @@ export default {
       const regex = typeof props.fileName === 'string' ? new RegExp(props.fileName, 'i') : props.fileName;
       if (props.forceFileName && props.fileName !== '' && !regex.test(event.target.files[0].name)) return emit('fileReadError',`Nombre de archivo erróneo,<br>se esperaba «${props.fileName}»`)
       if (props.maxFileSize!=null) if (event.target.files[0].size>props.maxFileSize)  return emit('fileReadError',`Archivo demasiado grande,<br>tamaño máximo ${props.maxFileSize/1024}KB`)
-      try {
-        emit('fileReaded', await readFile(event.target.files[0]));
-      } catch (err) {
-        emit('fileReadError',err.message);
-      }
+      try { emit('fileReaded', await readFile(event.target.files[0]))}
+      catch (err) { emit('fileReadError',err.message) }
       event.target.value='';
     };
 
@@ -49,8 +47,8 @@ export default {
 };
 </script>
 
-<style>
-.file-select{ position: relative; display: inline-flex; cursor: pointer; width: 100%; height: 3.125rem; justify-content: center; }
-input[type="file"] {display: none;}
-label{ width: 100%; height: 100%; padding-top: .625rem;}
+<style scoped>
+  div   { position: relative; display: inline-flex; cursor: pointer; width: 100%; height: 3.125rem; justify-content: center; }
+  input { display: none; }
+  label { width: 100%; height: 100%; padding-top: .625rem; }
 </style>
