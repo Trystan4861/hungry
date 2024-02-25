93333d9af173c7f6da6c5af4fd20e0038596c568
Trystan4861 <trystan4861@gmail.com>
2024-02-25 23:42:00 +0100

    css

--- src/components/MySelect.vue
+++ src/components/MySelect.vue
@@ -41,6 +41,7 @@ export default {
     onMounted(() => {
       if (props.selected.id !== -1) {
         selectedOption.value = props.selected;
+        emit('select',props.selected)
       }
     });
     return { showDropdown, selectedOption, toggleDropdown, selectOption, closeDropdown };
@@ -50,12 +51,15 @@ export default {
 </script>
 
 <style scoped>
+*{
+  color: white;
+}
 .my-select { position: relative; width: 100%; }
 .selected-option { padding-left: .625rem; background-color: gray; border: .0625rem solid #ccc; cursor: pointer; height: 3.125rem; flex-wrap: wrap; align-items: center; display: flex; }
-.dropdown { position: absolute; top: 100%; left: 0; width: 100%; background-color: gray; border: .0625rem solid #ccc; border-top: none; border-radius: 0 0 .25rem .25rem; box-shadow: 0 .125rem .25rem rgba(0, 0, 0, 0.1); z-index: 1000; display: none; }
+.dropdown { position: absolute; top: 100%; left: 0; width: 100%; background-color: gray; border: .0625rem solid #ccc; border-top: none; border-radius: 0 0 .25rem .25rem; box-shadow: 0 .125rem .25rem rgba(0, 0, 0, 0.1); z-index: 9999; display: none; }
 .dropdown.show { display: block; }
-.dropdown .option { padding-left: .625rem; padding-bottom: .625rem; cursor: pointer; }
-.dropdown .option:hover { background-color: lightgray; color: black; }
-.option { max-height: 3.125rem; }
-.my-select img { max-height: 2.8125rem; }
+
+.option{ max-height: 3.125rem; padding-top: .625rem; padding-left: .625rem; padding-bottom: .625rem; cursor: pointer; justify-content: flex-start; align-items: center; display: flex; }
+.option:hover { background-color: lightgray; color: black; }
+img { max-height: 2.8125rem; margin-right: .625rem;}
 </style>
