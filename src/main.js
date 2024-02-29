8bd6f7da221b627037df87a90207d21d577e8022
Trystan4861 <trystan4861@gmail.com>
2024-02-29 12:34:51 +0100

    fix deploy error

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@sweetalert2/theme-bootstrap-4/bootstrap-4.scss'; 
 
 const app=createApp(App);
 app.use(store);
