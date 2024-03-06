609eab91342b7879ed874cc6c7f0a1df8dee5e42
Trystan4861 <trystan4861@gmail.com>
2024-03-06 21:59:46 +0100

    test new index.html

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,26 @@
-const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = defineConfig({
+module.exports = {
   transpileDependencies: true,
   chainWebpack: config => {
     config.resolve.alias.set('@assets', path.resolve(__dirname, 'src/assets'));
+    config.resolve.alias.set('@', path.resolve(__dirname, 'src'));
+    config.resolve.alias.set('@components', path.resolve(__dirname, 'src/components'));
+  },
+  configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'index.html', // Nombre del archivo de salida
+        template: 'public/index.html', // Ruta del archivo HTML de entrada
+        inject: true, // Inyectar automáticamente los scripts generados
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeAttributeQuotes: true
+          // Otros ajustes de minificación si son necesarios
+        }
+      })
+    ]
   }
-});
+};
