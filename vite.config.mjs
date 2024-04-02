1fa03ed4faefd6a2faacda0776384a274b935185
Trystan4861 <trystan4861@gmail.com>
2024-04-02 15:36:35 +0200

    pwa

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { readFileSync } from 'fs';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -11,7 +12,14 @@ export default defineConfig({
       transformIndexHtml() {
         return readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
       },
-    }
+    },
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}']
+     },
+    })
   ],
   resolve: {
     alias: {
