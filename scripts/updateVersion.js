f0a1ff169925309a595db1f8ca3c5990ffadbc33
Trystan4861 <trystan4861@gmail.com>
2024-04-04 15:09:35 +0200

    refactor updateVersion.js

--- scripts/updateVersion.js
+++ scripts/updateVersion.js
@@ -4,27 +4,21 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 
 // Obtener la versión actual desde package.json
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
-let currentVersion = packageJson.version;
+const packageJson = require('./package.json');
 
-// Incrementar la versión según las reglas de SemVer
-const versionParts = currentVersion.split('.');
-let major = versionParts[0];
-let minor = versionParts[1];
-let patch = parseInt(versionParts[2]) + 1;
-if (patch > 99) {
-  patch = 0;
-  minor = parseInt(minor) + 1;
-}
-if (minor > 99) {
-  minor = 0;
-  major = parseInt(major) + 1;
-}
-const newVersion = `${major}.${minor}.${patch}`;
 
+const [,, strategy] = process.argv;
+let { major, minor, patch } = packageJson.version.split('.').map(Number);
+
+patch+=(strategy.toLowerCase() === 'minor')?(100-patch):1
+
+// Ajustar las partes de la versión si es necesario
+minor += Math.floor(patch / 100);
+major += Math.floor(minor / 100);
+
+packageJson.version = `${major}.${minor % 100}.${patch % 100}`;
+packageJson.revision = new Date().toISOString().slice(0, 16).replace(/(-|:)/ig,"").replace(/t/i,".")
 // Actualizar la versión en package.json
-packageJson.version = newVersion;
 fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
-
 // Añadir los cambios al área de preparación de Git y realizar una confirmación con un mensaje automático
-execSync(`git add package.json && git commit -m "Bump version to ${newVersion}"`);
+execSync(`git add package.json && git commit -m "Bump version to ${packageJson.version}-${packageJson.revision}"`);
