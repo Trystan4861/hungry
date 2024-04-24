2dfa3ab8700c8a25778282f4d7225e95a53ecfd7
Trystan4861 <trystan4861@gmail.com>
2024-04-24 12:57:15 +0200

    add SASS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
-import Vue3TouchEvents from "vue3-touch-events";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Vue3TouchEvents from "vue3-touch-events"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/Style.scss'
 import './registerServiceWorker'
+
 
 const app=createApp(App);
 app.use(store);
 app.use(Vue3TouchEvents);
+app.config.productionTip = false;
 app.mount('#app')
