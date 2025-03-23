66e226af0fbb02f7765e72da80a941ac10348043
Manuel Palanco <Trystan4861@gmail.com>
2025-03-23 02:22:09 +0100

    refactor listas de productos

--- pages/categorias.vue
+++ pages/categorias.vue
@@ -1,19 +1,3 @@
-<script setup lang="ts">
-import { myStore } from "~/composables/useStore";
-
-const { sortedByCategory, toggleSelected } = myStore();
-</script>
-
 <template>
-  <div>
-    <h1>Lista por Categoría</h1>
-    <ul>
-      <li v-for="item in sortedByCategory" :key="item.id">
-        {{ item.text }} ({{ item.id_categoria }})
-        <button @click="toggleSelected(item.id)">
-          {{ item.selected ? "✅" : "🛒" }}
-        </button>
-      </li>
-    </ul>
-  </div>
+  <MyProductListView orderType="category"/>
 </template>
